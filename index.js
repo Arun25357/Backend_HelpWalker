d157c2340e2b5309be39429c2112d2fec5550d9f
Fitywe <96216623+Arun25357@users.noreply.github.com>
2025-03-02 13:47:19 +0700

    update

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const mongoose = require('mongoose');
 const expressSession = require('express-session');
 const flash = require('connect-flash');
 
+const indexController = require('./controllers/indexController');
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded());
 app.use(express.json());
+
+app.get('/',indexController);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
